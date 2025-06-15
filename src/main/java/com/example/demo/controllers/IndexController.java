db4eefb98e65e09ced266771cdfa3c5d2dc54cc9
ShreyasVH <shreyas.deve.1@gmail.com>
2025-06-15 20:03:28 +0530

    added cli scripts

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -2,15 +2,16 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class IndexController
 {
     @GetMapping("/api")
-    public String get(@RequestParam(value = "input") String input)
+    public Map<String, String> get(@RequestParam(value = "input") String input)
     {
-        return "GET REQUEST with input: " + input;
+        return new HashMap<String, String>(){{put("input", input);}};
     }
 
     @PostMapping("/api")
